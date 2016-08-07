476e43adb9a57aca951922ad5a268815f9f1181e
naturegirl <jjguo@princeton.edu>
2016-08-06 20:05:16 -0700

    add code to create device -> events file, and add file

--- code/convert-files.py
+++ code/convert-files.py
@@ -16,9 +16,7 @@ def generate_event_apps_dict_file():
             event_apps_dict[event_id].append(app_id)
         else:
             event_apps_dict[event_id] = [app_id]
-    with open("../processed_data/event_apps.txt", "w") as f:
-        for k,v in event_apps_dict.items():
-            print(k, ":", ','.join(str(x) for x in v), sep='', file=f)
+    write_dict_to_file(event_apps_dict, "../processed_data/event_apps.txt")
     print("done")
 
 def generate_app_labels_dict_file():
@@ -32,7 +30,27 @@ def generate_app_labels_dict_file():
             app_labels_dict[app_id].append(label_id)
         else:
             app_labels_dict[app_id] = [label_id]
-    with open("../processed_data/app_labels.txt", "w") as f:
-        for k,v in app_labels_dict.items():
-            print(k, ":", ','.join(str(x) for x in v), sep='', file=f)
+    write_dict_to_file(app_labels_dict, "../processed_data/app_labels.txt")
     print("done")
+
+def generate_device_events_dict_file():
+    """generates file with mapping device_id -> [event_ids]"""
+    events = pd.read_csv("../data/events.csv")
+    device_events_dict = {}
+    for row in events.itertuples():
+        event_id = row[1]
+        device_id = row[2]
+        if device_id in device_events_dict:
+            device_events_dict[device_id].append(event_id)
+        else:
+            device_events_dict[device_id] = [event_id]
+    write_dict_to_file(device_events_dict, "../processed_data/device_events.txt")
+    print("done")
+
+def write_dict_to_file(d, fpath):
+    """write dict d to file given by path fpath"""
+    with open(fpath, "w") as f:
+        for k,v in d.items():
+            print(k, ":", ','.join(str(x) for x in v), sep='', file=f)
+
+generate_device_events_dict_file()
