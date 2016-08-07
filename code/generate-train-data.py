da58e88cdaf4900bf954230e6aae58f20c208478
naturegirl <jjguo@princeton.edu>
2016-08-06 20:43:19 -0700

    add optimized version to get event ids from device id

--- code/generate-train-data.py
+++ code/generate-train-data.py
@@ -8,32 +8,32 @@ import numpy as np
 import os
 import pandas as pd
 import pickle
+import time
 
 PICKLE_OUTPUT_FILE = "../processed_data/pickle/data.pickle"
 
-def read_event_apps_dict():
+def read_dict_file(fpath):
     d = {}
-    with open("../processed_data/event_apps.txt", "r") as f:
+    with open(fpath, "r") as f:
         for line in f:
-            event_id, apps = line.rstrip().split(":")
-            app_ids = [int(s) for s in apps.split(",")]
-            d[int(event_id)] = app_ids
+            key, vals = line.rstrip().split(":")
+            vals_a = [int(s) for s in vals.split(",")]
+            d[int(key)] = vals_a
     return d
 
-def read_app_labels_dict():
-    d = {}
-    with open("../processed_data/app_labels.txt", "r") as f:
-        for line in f:
-            app_id, labels = line.rstrip().split(":")
-            label_ids = [int(s) for s in labels.split(",")]
-            d[int(app_id)] = label_ids
-    return d
+
+def get_event_ids_optimized(device_id, device_events_dict):
+    """gets list of event_ids matching given devide_id"""
+    if device_id in device_events_dict:
+        return device_events_dict[device_id]
+    else:
+        return []
 
 def get_event_ids(device_id, events):
     """gets list of event_ids matching given devide_id"""
     return events[events["device_id"] == device_id]["event_id"].tolist()
 
-def get_app_ids_optimized(event_ids, event_apps_dict):
+def get_app_ids(event_ids, event_apps_dict):
     """get list of unique installed app ids based on event_apps_dict."""
     app_ids = set()
     for event_id in event_ids:
@@ -42,7 +42,7 @@ def get_app_ids_optimized(event_ids, event_apps_dict):
                 app_ids.add(app_id)
     return list(app_ids)
 
-def get_label_ids_optimized(app_ids, app_labels_dict):
+def get_label_ids(app_ids, app_labels_dict):
     """get list of unique category label ids based on installed app ids and dict app_id -> app_label."""
     label_ids = set()
     for app_id in app_ids:
@@ -53,8 +53,9 @@ def get_label_ids_optimized(app_ids, app_labels_dict):
 
 
 # Read Data
-event_apps_dict = read_event_apps_dict()
-app_labels_dict = read_app_labels_dict()
+event_apps_dict = read_dict_file("../processed_data/event_apps.txt")
+app_labels_dict = read_dict_file("../processed_data/app_labels.txt")
+device_events_dict = read_dict_file("../processed_data/device_events.txt")
 events = pd.read_csv("../data/events.csv")
 train = pd.read_csv("../data/gender_age_train.csv")
 phone_device = pd.read_csv("../data/phone_brand_device_model.csv")
@@ -72,12 +73,16 @@ df.drop(["device_model", "phone_brand"], inplace=True, axis=1)
 
 # Join with event_ids, app_ids and app_labels
 print("start map")
+print(time.time())
+#df["event_ids"] = df["device_id"].map(lambda _id: get_event_ids_optimized(_id, device_events_dict))
 df["event_ids"] = df["device_id"].map(lambda _id: get_event_ids(_id, events))
 print("done map 1")
-df["app_ids"] = df["event_ids"].map(lambda event_ids: get_app_ids_optimized(event_ids, event_apps_dict))
+print(time.time())
+df["app_ids"] = df["event_ids"].map(lambda event_ids: get_app_ids(event_ids, event_apps_dict))
 print("done map 2")
-df["label_ids"] = df["app_ids"].map(lambda app_ids: get_label_ids_optimized(app_ids, app_labels_dict))
-
+print(time.time())
+df["label_ids"] = df["app_ids"].map(lambda app_ids: get_label_ids(app_ids, app_labels_dict))
+print(time.time())
 print("start pickling")
 pickle.dump(df, open(PICKLE_OUTPUT_FILE, "wb"), protocol=pickle.HIGHEST_PROTOCOL)
 
