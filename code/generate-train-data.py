8f8b762f6a0f9306d6d3418bbefc0e8196a6a24b
naturegirl <jennifer.jinyuan.guo@gmail.com>
2016-08-26 03:29:11 +0200

    optimize generate-train-data

--- code/generate-train-data.py
+++ code/generate-train-data.py
@@ -8,7 +8,6 @@ import numpy as np
 import os
 import pandas as pd
 import pickle
-import time
 
 PICKLE_OUTPUT_FILE = "../processed_data/pickle/data.pickle"
 
@@ -21,6 +20,13 @@ def read_dict_file(fpath):
             d[int(key)] = vals_a
     return d
 
+def read_agegroups():
+    d = {}
+    with open("../processed_data/agegroups.txt", "r") as f:
+        for line in f:
+            idx, group = line.rstrip().split(":")
+            d[group] = int(idx)
+    return d
 
 def get_event_ids_optimized(device_id, device_events_dict):
     """gets list of event_ids matching given devide_id"""
@@ -56,7 +62,7 @@ def get_label_ids(app_ids, app_labels_dict):
 event_apps_dict = read_dict_file("../processed_data/event_apps.txt")
 app_labels_dict = read_dict_file("../processed_data/app_labels.txt")
 device_events_dict = read_dict_file("../processed_data/device_events.txt")
-events = pd.read_csv("../data/events.csv")
+agegroups_dict = read_agegroups()
 train = pd.read_csv("../data/gender_age_train.csv")
 phone_device = pd.read_csv("../data/phone_brand_device_model.csv")
 brand_dict = pd.read_csv("../processed_data/phone_brands.csv", index_col=0).to_dict()['phone_brand_id']
@@ -66,23 +72,18 @@ print("done reading data")
 # Join with phone device data
 df = pd.merge(train, phone_device, on='device_id')
 print(df.shape)
-print(len(df))
 df["phone_brand_id"] = df["phone_brand"].map(lambda s: brand_dict[s])
 df["device_model_id"] = df["device_model"].map(lambda s: model_dict[s])
+df["group_id"] = df["group"].map(lambda s: agegroups_dict[s])
 df.drop(["device_model", "phone_brand"], inplace=True, axis=1)
 
 # Join with event_ids, app_ids and app_labels
 print("start map")
-print(time.time())
-#df["event_ids"] = df["device_id"].map(lambda _id: get_event_ids_optimized(_id, device_events_dict))
-df["event_ids"] = df["device_id"].map(lambda _id: get_event_ids(_id, events))
+df["event_ids"] = df["device_id"].map(lambda _id: get_event_ids_optimized(_id, device_events_dict))
 print("done map 1")
-print(time.time())
 df["app_ids"] = df["event_ids"].map(lambda event_ids: get_app_ids(event_ids, event_apps_dict))
 print("done map 2")
-print(time.time())
 df["label_ids"] = df["app_ids"].map(lambda app_ids: get_label_ids(app_ids, app_labels_dict))
-print(time.time())
 print("start pickling")
 pickle.dump(df, open(PICKLE_OUTPUT_FILE, "wb"), protocol=pickle.HIGHEST_PROTOCOL)
 
